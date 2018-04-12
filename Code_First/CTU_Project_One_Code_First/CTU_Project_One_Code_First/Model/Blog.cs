5bad71044d71addca5bc0a733c9c573c2462e715
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2018-04-12 15:32:48 +0200

    project one

---
+++ Code_First/CTU_Project_One_Code_First/CTU_Project_One_Code_First/Model/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTU_Project_One_Code_First.Model
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+}
---
+++ Code_First/CTU_Project_One_Code_First/CTU_Project_One_Code_First/Model_DB_Context/BloggingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTU_Project_One_Code_First.Model;
+
+namespace CTU_Project_One_Code_First.Model_DB_Context
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
